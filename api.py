63eb3ab1908674315d7efca0c97ee2b5e307e6d3
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-03-02 00:17:41 +0500

    data_base

--- api.py
+++ api.py
@@ -1,3 +1,7 @@
 import requests
 
 base_url = "https://randomuser.me/api/"
+r=requests.get(base_url)
+data=r.json()
+data=data['results'][0]
+#print(data[0]['name']['first'])
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from tinydb import TinyDB
-
-db = TinyDB('data.json')
+import api
+data=api.data
+db = TinyDB('data.json', intent=4)
+doc={
+    "first_name": f"{data['name']['first']}",
+    "last_name": f"{data['name']['last']}",
+    "age": data['dob']['age'],
+    "phone": f"{data['phone']}",
+    "country": f"{data['location']['country']}",
+    "email": f"{data['email']}",
+}
+db.insert(doc)
