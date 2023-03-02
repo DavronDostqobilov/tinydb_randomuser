a64531fc1a17d570ab99104d8208ab5ee7a50924
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-03-02 17:04:08 +0500

    tinydb_randomer

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
-import requests
+
 
 base_url = "https://randomuser.me/api/"
-r=requests.get(base_url)
-data=r.json()
-data=data['results'][0]
-#print(data[0]['name']['first'])
+
+#print(data(5))
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from tinydb import TinyDB
+import requests
 import api
-data=api.data
-db = TinyDB('data.json', intent=4)
-doc={
-    "first_name": f"{data['name']['first']}",
-    "last_name": f"{data['name']['last']}",
-    "age": data['dob']['age'],
-    "phone": f"{data['phone']}",
-    "country": f"{data['location']['country']}",
-    "email": f"{data['email']}",
-}
-db.insert(doc)
+
+db = TinyDB('data.json', indent=4)
+url=api.base_url
+def data(n):
+    param={
+        'X-Api-Key':'7b3b6035a5c94aee8908aaa68c9a6fe4',
+        'results':n
+    }
+    r=requests.get(url,params=param)
+    data=r.json()
+    return data['results']
+
+data=data(50)
+
+
+for i in range(len(data)):
+    doc={
+        "first_name": f"{data[i]['name']['first']}",
+        "last_name": f"{data[i]['name']['last']}",
+        "age": data[i]['dob']['age'],
+        "phone": f"{data[i]['phone']}",
+        "country": f"{data[i]['location']['country']}",
+        "email": f"{data[i]['email']}",
+    }
+    db.insert(doc)
